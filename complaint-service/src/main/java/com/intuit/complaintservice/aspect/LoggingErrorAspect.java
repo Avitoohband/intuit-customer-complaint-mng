b1e449c532c4e75456c91d2652d1b4551759aca0
Avi Tuchband <avitoohband@gmail.com>
2023-11-27 20:31:35 +0200

    name fixes

--- complaint-service/src/main/java/com/intuit/complaintservice/aspect/LoggingErrorAspect.java
+++ complaint-service/src/main/java/com/intuit/complaintservice/aspect/LoggingErrorAspect.java
@@ -11,7 +11,7 @@ import org.springframework.stereotype.Component;
 @Aspect
 @Component
 @Slf4j
-public class LogErrorAspect {
+public class LoggingErrorAspect {
 
     @Pointcut("execution(* com.intuit.complaintservice.controller.*.*(..))")
     public void controllerPointcut() {
